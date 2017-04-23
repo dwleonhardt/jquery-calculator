2c8b7e8071d02b6b760f06a3e016a2b0340edbaa
dwleonhardt <dwleonhardt@gmail.com>
2017-04-23 17:45:56 -0600

    main operands work

---
+++ js/index.js
@@ -0,0 +1,49 @@
+var buttons = $('#buttons-container')[0];
+var calcScreen = $('#screen');
+var number1 = [];
+var number2 = [];
+var current = '0';
+var operator = '';
+$(buttons).on('click', function(event) {
+  $target = $(event.target);
+  if ($target.text().length <= 1) {
+    if (isNaN($target.text()) === false) {
+      current += $target.text();
+      // console.log(current);
+    } else if($target.text() === '=') {
+      number2.push(current);
+      if (operator === '+') {
+        add(number1, number2);
+      } else if (operator === '÷') {
+        divide(number1, number2);
+      } else if (operator === 'x') {
+        multiply(number1, number2);
+      } else if (operator === '-') {
+        subtract(number1, number2);
+      } 
+    } else {
+        number1.push(current);
+        current = '0';
+        operator = $target.text();
+    }
+  }
+  console.log(number1[0]);
+  console.log(number2[0]);
+});
+
+function add(number1, number2) {
+  calcScreen.text(parseInt(number1[0]) + parseInt(number2[0]));
+  number1[0] = parseInt(number1[0]) + parseInt(number2[0]);
+}
+function subtract(number1, number2) {
+  calcScreen.text(parseInt(number1[0]) - parseInt(number2[0]));
+  number1[0] = parseInt(number1[0]) - parseInt(number2[0]);
+}
+function multiply(number1, number2) {
+  calcScreen.text(parseInt(number1[0]) * parseInt(number2[0]));
+  number1[0] = parseInt(number1[0]) * parseInt(number2[0]);
+}
+function divide(number1, number2) {
+  calcScreen.text(parseInt(number1[0]) / parseInt(number2[0]));
+  number1[0] = parseInt(number1[0]) / parseInt(number2[0]);
+}
